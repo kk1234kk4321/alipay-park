4ae4fa859a47593d8652a7681175b4057cf8a1e2
xiaqt1984 <182001560@qq.com>
2018-06-27 16:09:13 +0800

    no message

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -7,3 +7,7 @@ require('../../pages/parkOwnerManage/parkOwnerManage');
 require('../../pages/updateFeeInfo/updateFeeInfo');
 require('../../pages/auditInfo/auditInfo');
 require('../../pages/parkingManage/parkingManage');
+require('../../pages/searchPark/searchPark');
+require('../../pages/parkInfo/parkInfo');
+require('../../pages/payFeets/payFeets');
+require('../../pages/paymentInfo/paymentInfo');
--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -9,5 +9,9 @@ require('../../pages/parkOwnerManage/parkOwnerManage');
 require('../../pages/updateFeeInfo/updateFeeInfo');
 require('../../pages/auditInfo/auditInfo');
 require('../../pages/parkingManage/parkingManage');
+require('../../pages/searchPark/searchPark');
+require('../../pages/parkInfo/parkInfo');
+require('../../pages/payFeets/payFeets');
+require('../../pages/paymentInfo/paymentInfo');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
