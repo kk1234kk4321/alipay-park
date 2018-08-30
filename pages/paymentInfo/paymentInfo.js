71798c6e054878d325f369a070dba9deb699341d
caoyong <1666416683@qq.com>
2018-08-30 14:35:45 +0800

    停车历史缴费记录：不超过500条数据，有多少显示多少条；超过500条数据，只显示最近500条数据

--- pages/paymentInfo/paymentInfo.js
+++ pages/paymentInfo/paymentInfo.js
@@ -1,7 +1,7 @@
 const app = getApp()
 Page({
   data: {
-    paymentInfo: {},
+    paymentInfo: [],
     carNo:''
   },
   onLoad(res) {
@@ -26,9 +26,22 @@ Page({
         console.log("调用历史缴费信息接口成功")
         var res = JSON.parse(res.data)
         console.log(res)
+        var a = 0;
         if (res.data) {
+          var ss = [];
+          var len = res.data.length;
+          console.log("总记录条数：", len);
+          if(len<=500) {//有多少条数据，显示多少条
+            for(a=0;a<len;a++) {
+              ss.push(res.data[a]);
+            }
+          } else {//只显示前500条数据
+            for(a=0;a<500;a++) {
+              ss.push(res.data[a]);
+            }
+          }
           that.setData({
-            paymentInfo: res.data
+            paymentInfo: ss
           })
         } else {
           that.setData({
